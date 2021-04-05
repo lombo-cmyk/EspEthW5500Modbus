6bbfe7b5ccc988ed4856a70e95273d76922dc50e
lombo-cmyk <srsrsrew@gmail.com>
2021-04-05 12:22:22 +0200

    Remove dead code

--- main/tcp_slave.cpp
+++ main/tcp_slave.cpp
@@ -5,26 +5,10 @@
    CONDITIONS OF ANY KIND, either express or implied.
 */
 #include <cstdio>
-#include <cstring>
 #include "esp_err.h"
-#include "sdkconfig.h"
 #include "esp_log.h"
 #include "esp_event.h"
 #include "nvs_flash.h"
-#include "esp_netif.h"
-
-#define MB_TCP_PORT_NUMBER (CONFIG_FMB_TCP_PORT_DEFAULT)
-#define MB_MDNS_PORT (502)
-
-#define MB_PAR_INFO_GET_TOUT (10) // Timeout for get parameter info
-#define MB_CHAN_DATA_MAX_VAL (10)
-#define MB_CHAN_DATA_OFFSET (0.1f)
-
-#define MB_READ_MASK \
-    (MB_EVENT_INPUT_REG_RD | MB_EVENT_HOLDING_REG_RD | MB_EVENT_DISCRETE_RD | \
-     MB_EVENT_COILS_RD)
-#define MB_WRITE_MASK (MB_EVENT_HOLDING_REG_WR | MB_EVENT_COILS_WR)
-#define MB_READ_WRITE_MASK (MB_READ_MASK | MB_WRITE_MASK)
 
 #include "include/EthernetW5500.h"
 #include "include/ModbusStructures.h"
@@ -36,7 +20,6 @@ void app_main();
 
 void app_main(void) {
     esp_log_level_set("*", ESP_LOG_VERBOSE);
-    ESP_LOGI(ModbusTag.c_str(), "FREE HEAP: %d", esp_get_free_heap_size());
 
     esp_err_t result = nvs_flash_init();
     if (result == ESP_ERR_NVS_NO_FREE_PAGES ||
@@ -49,9 +32,8 @@ void app_main(void) {
     auto& ethManager = EthernetW5500::getInstance();
     auto& modbusManager = Modbus::getInstance();
 
-    ESP_LOGI(ModbusTag.c_str(), "FREE HEAP: %d", esp_get_free_heap_size());
     static uint8_t ucParameterToPass;
-    TaskHandle_t xHandle = NULL;
+    TaskHandle_t xHandle = nullptr;
     xTaskCreate(Modbus::RunSlaveTask,
                 "Modbus_task",
                 2048,
